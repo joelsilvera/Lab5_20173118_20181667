aebb7eb732ad58a99f7c223d203806b21074fc89
joelsilvera <joel.silvera@pucp.edu.pe>
2023-06-11 15:20:55 -0500

    Este es el primer commit

---
+++ app/src/main/java/com/example/lab5_20173118_20181667/NuevaCuenta.java
@@ -0,0 +1,14 @@
+package com.example.lab5_20173118_20181667;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevaCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_cuenta);
+    }
+}
